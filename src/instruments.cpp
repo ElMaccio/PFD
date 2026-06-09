5bd9ef5775f0d736daa9ed91ab4dd8dc46fe39b6
Elmaccio <maciej.maniakowskii@gmail.com>
2026-06-09 17:32:48 +0200

    Added communication with esp

---
+++ src/instruments.cpp
@@ -0,0 +1,45 @@
+#include "instruments.h"
+
+int Instruments::initInstruments() {
+    if (gpioInitialise() < 0) {
+        fprintf(stderr, "pigpio initialization failed\n");
+        return 1;
+    }
+
+    spi_handle = spiOpen(0, 1000000, 0);
+    if (spi_handle < 0) {
+        fprintf(stderr, "Failed to open SPI channel\n");
+        gpioTerminate();
+        return 1;
+    }
+
+    bufferSize = NumInstruments * 5;
+
+    for(int i=0;i<bufferSize;i++) 
+            tx_buf[i]=0xFF;        
+
+    return 0;
+}
+
+void Instruments::readInstruments() {
+    spiXfer(spi_handle, tx_buf, rx_buf, bufferSize);
+
+    for (int i = 0; i < NumInstruments; i++)
+    {
+            instruments[i].idWithInop = rx_buf[i * 5];
+
+            memcpy(&instruments[i].value, &rx_buf[i * 5 + 1], sizeof(float));
+
+            //printf("ID: 0x%02X, Value: %f\n", instruments[i].idWithInop, instruments[i].value);
+    }
+}
+
+float Instruments::getInstrumentValue(int type) const {
+    if (type < 0 || type >= NumInstruments) return 0.0f;
+    return instruments[type].value;
+}
+
+void Instruments::cleanup() {
+    spiClose(spi_handle);
+    gpioTerminate();
+}
---
+++ src/instruments.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pigpio.h>
+#include <cstring>
+
+//#define BUFFER_SIZE 64
+
+class Instruments {
+    public:
+        enum Type{
+            BatteryVoltage,
+            Altitude,
+            Airspeed,
+            Temperature1,
+            Temperature2,
+            Roll,
+            Pitch,
+            NumInstruments
+        };
+
+        struct InstrumentData
+        {
+            uint8_t idWithInop;
+            float value;
+        } __attribute__((packed));
+
+        int initInstruments();
+        void readInstruments();
+        float getInstrumentValue(int type) const;
+        void cleanup();
+
+        private:
+            int spi_handle;
+            InstrumentData instruments[NumInstruments];
+            char tx_buf[NumInstruments * 5];
+            char rx_buf[NumInstruments * 5];
+            int bufferSize;
+};
